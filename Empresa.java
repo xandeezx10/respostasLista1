d98d75b1d1d790e861a2241333818116e4be0864
xandeezx10 <166646835+xandeezx10@users.noreply.github.com>
2024-04-10 16:52:30 -0300

    Create Empresa.java

---
+++ Empresa.java
@@ -0,0 +1,32 @@
+public class Empresa {
+	private String nome;
+	private String cnpj;
+	public Empresa(String n, String cnpj) {
+		this.nome = n;
+		this.cnpj = cnpj;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getNome() {
+		return nome = nome;
+	}
+	public String getCnpj() {
+		return cnpj = cnpj;
+	}
+	public void setNome(){
+		this.nome = nome;
+	}
+	public void setCnpj() {
+		this.cnpj = cnpj;
+	}
+}
